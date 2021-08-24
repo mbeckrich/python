000234125301bf134f0b71fb5e3b48be135cc64d
marshall beckrich <mkbeckrich@icloud.com>
2021-08-23 22:14:57 -0600

    finished practical python 1

--- exercises/practice-python/exercise-1.py
+++ exercises/practice-python/exercise-1.py
@@ -2,11 +2,18 @@
 # Print out a message addressed to them that tells them the year that they will turn 100 years old.
 
 name = input("Please tell me your name: \n")  # \n causes a linebreak
-print("Thanks! Hi, " + name)
+print("Thanks! Hi, " + name + ".")
 
 age = int(input("And how old are you, " + name + "? \n"))
 print(name + ", you are " + str(age) + " years old.")
 
 i = int(100)
 old_age = i - age
-print("You will be 100 in " + str(old_age) + " years.")
+current_year = 2021
+print(
+    "You will be 100 in "
+    + str(old_age)
+    + " years. That'll be in "
+    + str(int(current_year + old_age))
+    + "."
+)
