8a3aab3aeaeb8747e98728d814dff1f5e26f1a54
mkb <mkbeckrich@icloud.com>
2022-03-21 00:03:43 -0600

    progress: 75% through day 22

---
+++ exercises/100-days-of-python/day-22/pong/ball.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+# width 20
+# height 20
+# x, y 0
+# move up and to right at start
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+
+    def ball_movement(self):
+        self.new_x = self.xcor() + self.x_move
+        self.new_y = self.ycor() + self.y_move
+        self.goto(self.new_x, self.new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- exercises/100-days-of-python/day-22/pong/main.py
+++ exercises/100-days-of-python/day-22/pong/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from bars import Bars
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -7,7 +9,7 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
-
+ball = Ball()
 right_bar = Bars((350, 0))
 left_bar = Bars((-350, 0))
 
@@ -19,6 +21,20 @@ screen.onkey(left_bar.down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.ball_movement()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (
+        ball.distance(right_bar) < 50
+        and ball.xcor() > 320
+        or ball.distance(left_bar) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
+
 
 screen.exitonclick()
