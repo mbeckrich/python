c2a0a213563333d9621d0a2c75857539453f74bd
mkb <mkbeckrich@icloud.com>
2022-04-06 16:35:50 -0600

    progress: day-26 halfway through

---
+++ exercises/100-days-of-python/day-26/main.py
@@ -0,0 +1,30 @@
+# keyword method:
+# name_for_new_list = [new_item for current_item in current_list]
+# numbers = [1, 2, 3]
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+
+# # With strings
+# name = "Marshall"
+# new_list = [letter for letter in name]
+# print(new_list)
+
+# # Conditional lists
+# names = ["alex", "beth", "caroline", "dave", "eleanor", "freddie"]
+# # short names = [new_item for current_item in current_list if test_passes]
+# short_names = [name.upper() for name in names if len(name) >= 5]
+# print(short_names)
+
+# with open("file1.txt") as file1:
+#     file1 = file1.readlines()
+
+
+# with open("file2.txt") as file2:
+#     file2 = file2.readlines()
+
+
+# result = [int(num) for num in file1 if num in file2]
+
+# # Write your code above 👆
+
+# print(result)
